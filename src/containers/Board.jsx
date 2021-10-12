c4bf1ca371ed8a021454d4d9d2bf7908c0857be1
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-12 19:11:36 +0545

    feat: display winner result

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Square from '../components/Square';
 import './board.css';
 
@@ -26,9 +26,49 @@ const Board = () => {
     setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
   }
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  useEffect(() => {
+    const w = calculateWinner(squares);
+    console.log(w);
+    if (w) {
+      setWinner(w);
+    }
+
+    if (!w && !squares.filter((square) => !square).length) {
+      setWinner('Both');
+    }
+  }, [squares]);
   return (
     <div>
-      <p>{currentPlayer} Players turn </p>
+      {!winner && <p>{currentPlayer} Players turn </p>}
+      {winner && winner !== 'Both' && <p>Congratulation {winner} </p>}
+      {winner && winner === 'Both' && (
+        <p>Congratulation you both are winner </p>
+      )}
       <div className='grid'>
         {Array(9)
           .fill(null)
