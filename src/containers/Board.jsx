4bd704469823d434de7489abc715270732b3d0b2
thaneshworjoshi <thaneshwor10@gmail.com>
2021-10-26 19:28:07 +0545

    simplified draw logic

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react";
-import Square from "../components/Square";
-import "./board.css";
+import React, { useEffect, useState } from 'react';
+import Square from '../components/Square';
+import './board.css';
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
   const [currentPlayer, setCurrentPlayer] = useState(
-    Math.round(Math.random() * 1) === 1 ? "X" : "O"
+    Math.round(Math.random() * 1) === 1 ? 'X' : 'O'
   );
   const [winner, setWinner] = useState(null);
 
   function reset() {
     setSquares(Array(9).fill(null));
     setWinner(null);
-    setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
+    setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? 'X' : 'O');
   }
 
   function setSquareValue(index) {
@@ -23,7 +24,7 @@ const Board = () => {
       return val;
     });
     setSquares(newData);
-    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
   }
 
   function calculateWinner(squares) {
@@ -54,43 +55,51 @@ const Board = () => {
   useEffect(() => {
     const w = calculateWinner(squares);
     console.log(w);
+
     if (w) {
       setWinner(w);
     }
 
-    if (!w && !squares.filter((square) => !square).length) {
-      setWinner("Both");
+    let nullArr = squares.filter((square) => {
+      if (square === null) {
+        return true;
+      }
+    });
+
+    if (!w && nullArr.length === 0) {
+      setWinner('Both');
     }
   }, [squares]);
+
   return (
-    <div className="body-section">
-      <div className="bg-blur"></div>
-      <div className="container">
-        <div className="player-turn">
+    <div className='body-section'>
+      <div className='bg-blur'></div>
+      <div className='container'>
+        <div className='player-turn'>
           {!winner && (
             <p>
-              <span className="current-player">{currentPlayer} : </span> players
-              turn{" "}
+              <span className='current-player'>{currentPlayer} : </span> players
+              turn
             </p>
           )}
         </div>
       </div>
 
-      {winner && winner !== "Both" && (
-        <div className="result-board">
+      {winner && winner !== 'Both' && (
+        <div className='result-board'>
           <p>Winner : {winner} </p>
         </div>
       )}
-      {winner && winner === "Both" && (
-        <div className="result-board">
+      {winner && winner === 'Both' && (
+        <div className='result-board'>
           <p>Draw</p>
         </div>
       )}
 
-      <div className="tc-wrapper">
-        <div className="container">
-          <div className="tc-top-section">
-            <div className="tc-box-wrapper">
+      <div className='tc-wrapper'>
+        <div className='container'>
+          <div className='tc-top-section'>
+            <div className='tc-box-wrapper'>
               {Array(9)
                 .fill(null)
                 .map((_, i) => {
@@ -105,8 +114,8 @@ const Board = () => {
                 })}
             </div>
           </div>
-          <div className="tc-bottom-section">
-            <button className="tc-reset" onClick={reset}>
+          <div className='tc-bottom-section'>
+            <button className='tc-reset' onClick={reset}>
               reset
             </button>
           </div>
